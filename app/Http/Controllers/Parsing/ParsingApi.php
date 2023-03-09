cd8e74db4330dc0e10b3ba95f381eedd0b15acc2
yzeinullaev <yzeinullaev@gmail.com>
2023-03-09 10:08:43 +0600

    parse

---
+++ app/Http/Controllers/Parsing/ParsingApi.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Parsing;
+
+use Illuminate\Routing\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ParsingApi extends Controller
+{
+
+    public function updateParsingData(Request $request)
+    {
+
+    }
+
+    public function getParsingData(Request $request)
+    {
+
+    }
+}
---
+++ app/Http/Requests/Parsing/UpdateParsingFormRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateParsingFormRequest extends FormRequest
+{
+
+}
